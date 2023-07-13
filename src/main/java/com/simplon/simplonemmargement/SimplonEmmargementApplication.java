969374b81a503cfd8ee661d1e989ff5b4867a7d7
Tonde Souleymane <tondesoulco@gmail.com>
2023-07-13 12:08:49 +0000

    first commit

---
+++ src/main/java/com/simplon/simplonemmargement/SimplonEmmargementApplication.java
@@ -0,0 +1,13 @@
+package com.simplon.simplonemmargement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplonEmmargementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplonEmmargementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simplon/simplonemmargement/SimplonEmmargementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.simplonemmargement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplonEmmargementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
